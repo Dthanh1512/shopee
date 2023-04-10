e635298223423556c828f41cd0eb7e9b0c1d1cac
[ETA] ThanhNHD <thanhnhd@ethan-tech.com>
2023-04-10 17:11:28 +0700

    update code

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  baseURL: 'https://api-ecom.duthanhduoc.com/',
+  baseUrl: 'https://api-ecom.duthanhduoc.com/',
   maxSizeUploadAvarta: 1048576
 }
 export default config
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosError, HttpStatusCode } from 'axios'
 import config from 'src/constants/config'
-import UserImage from 'src/assets/images/user.svg'
+import userImage from 'src/assets/images/user.svg'
+import { ErrorResponse } from 'src/types/utils.type'
 
 export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
   // eslint-disable-next-line import/no-named-as-default-member
@@ -9,6 +10,17 @@ export function isAxiosError<T>(error: unknown): error is AxiosError<T> {
 
 export function isAxiosUnprocessableEntityError<FormError>(error: unknown): error is AxiosError<FormError> {
   return isAxiosError(error) && error.response?.status === HttpStatusCode.UnprocessableEntity
+}
+
+export function isAxiosUnauthorizedError<UnauthorizedError>(error: unknown): error is AxiosError<UnauthorizedError> {
+  return isAxiosError(error) && error.response?.status === HttpStatusCode.Unauthorized
+}
+
+export function isAxiosExpiredTokenError<UnauthorizedError>(error: unknown): error is AxiosError<UnauthorizedError> {
+  return (
+    isAxiosUnauthorizedError<ErrorResponse<{ name: string; message: string }>>(error) &&
+    error.response?.data?.data?.name === 'EXPIRED_TOKEN'
+  )
 }
 
 export function formatCurrency(currency: number) {
@@ -39,4 +51,5 @@ export const getIdFromNameId = (nameId: string) => {
   const arr = nameId.split('-i-')
   return arr[arr.length - 1]
 }
-export const getAvatarUrl = (avartaName?: string) => (avartaName ? `${config.baseURL}images/${avartaName}` : UserImage)
+
+export const getAvatarUrl = (avatarName?: string) => (avatarName ? `${config.baseUrl}images/${avatarName}` : userImage)
